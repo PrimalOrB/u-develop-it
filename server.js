dd05bf850d237db1929504e5e7b5af01d2829080
Andrew Ogilvie <primalorb@gmail.com>
2021-05-04 20:11:43 -0400

    add sql queries initial

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 const express = require( 'express' );
+const mysql = require( 'mysql2' );
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use( express.urlencoded( {extended: false} ) );
 app.use( express.json() );
+
+const db = mysql.createConnection( 
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election',
+    },
+    console.log( 'Connected to the election database' )
+)
+
+// get all candidates
+// db.query( `SELECT * FROM candidates`, ( err, rows ) => {
+//     console.log( rows )
+// })
+
+// get a single candidate
+// db.query( `SELECT * FROM candidates WHERE id = 1`, ( err, row ) => {
+//     if ( err ) {
+//         console.log( err );
+//     }
+//     console.log( row );
+// });
+
+// delete a candidate
+// db.query( `DELETE FROM candidates WHERE id = ?`, 1, ( err, result ) => {
+//     if ( err ) {
+//         console.log( err );
+//     }
+//     console.log( result );
+// });
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES (?,?,?,?)`;
+const params = [ 1, 'Ronald', 'Firbank', 1 ];
+
+db.query( sql, params, ( err, result ) => {
+    if ( err ) {
+        console.log( err )
+    }
+    console.log( result )
+})
+
 
 // catch all 404 error
 app.use( ( req, res ) => {
