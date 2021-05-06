eb64125db74c2943e13e38f364a987147e862a6c
Andrew Ogilvie <primalorb@gmail.com>
2021-05-06 16:56:56 -0400

    add route and query to tabulate results

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: '', // remove for github pushing
         database: 'election',
     },
     console.log( 'Connected to the election database' )
